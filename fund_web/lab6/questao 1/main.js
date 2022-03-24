ec2bf4a74a6643811af86ba0063401eff665420e
Ana Julia Lemos <95362082+Anajulialemos@users.noreply.github.com>
2022-03-24 20:42:08 -0300

    Add files via upload

---
+++ fund_web/lab6/questao 1/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function(){
+    var number = 12;
+    var numberStart = number;
+
+    if (number < 1)
+        window.alert("Desculpe mas números fatoriais tem que ser maiores que 0");
+    else{
+        for(let i = number-1; i > 1; i-- ){
+            number = number * i;
+        }
+        window.alert(numberStart + "!" + " = " + number);
+    }    
+});
