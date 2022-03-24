cf51d23ef7b74f2c30e00092804443d967e0b4f4
Ana Julia Lemos <95362082+Anajulialemos@users.noreply.github.com>
2022-03-24 20:57:52 -0300

    Delete questao5.js

--- fund_web/lab6/questao5.js
+++
@@ -1,7 +0,0 @@
-function numeroPrimo(numero) {
-    for (let divisor = 2; divisor < numero; divisor++) 
-    if (numero % divisor == 0) return false;
-    return true;
-}
-for (let i = 2; i < 1000; i++) if (numeroPrimo(i))
- console.log(i);
